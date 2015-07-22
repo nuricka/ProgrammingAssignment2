a0915ae1ce7b2ef71422263baf88f0ef5e7fe997
nuricka <nuricka@cmail.cat>
2015-07-23 00:19:25 +0200

    filling cacheMatrix.R with code to pass the project assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+################################################################################
+#                                                                              #
+#                        makeCacheMatrix - cacheSolve                          #
+#                                                                              #
+#    Functions that is able to cache the inverse of an invertible matrix       #
+#    (the matrix supplied is always invertible) to avoid repeating the         #
+#    time-consuming computation of calculating the inverse.                    #
+#                                                                              #
+################################################################################
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can cache:
+#  * its inverse to avoid recalculating in the future.
+#  * the matrix itself to determine whether the matrix has changed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+  }
+  get <- function() x
+  setmat <- function(z) mat <<- z
+  getmat <- function() mat
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv, getinv = getinv,
+       setmat = setmat, getmat = getmat)  
 }
 
 
-## Write a short comment describing this function
+# cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix
+# only when necessary
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  m <- x$get()
+  if(!is.null(inv))  # does the inverse exist?
+  {
+    mat<-x$getmat()
+    if (identical(m,mat)) # has the matrix changed?
+    {
+      message("getting cached data to restore the inverse of the matrix")
+      return(inv)
+    }
+  }
+  #data <- x$get()
+  inv <- solve(m, ...)
+  # store the new values for matrix and inverse
+  x$set(m) 
+  x$setmat(m)
+  x$setinv(inv) 
+  inv
+  
 }
